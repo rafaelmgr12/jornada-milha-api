5ae907d6a39fc61434be0284bdf028c0da69127e
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-07-25 18:34:28 -0300

    [fix] erro in the return of get

--- internal/domain/gateway/testimonial.go
+++ internal/domain/gateway/testimonial.go
@@ -1,10 +1,14 @@
 package gateway
 
-import "context"
+import (
+	"context"
+
+	"github.com/rafaelmgr12/jornada-milha-api/internal/domain/entity"
+)
 
 type TestimonialGateway interface {
 	CreateTestimonial(ctx context.Context, name string, testimonial string) error
-	ReadTestimonial(ctx context.Context, id string) (string, string, error)
+	ReadTestimonial(ctx context.Context) ([]*entity.Testimonial, error)
 	UpdateTestimonial(ctx context.Context, id string, name string, testimonial string) error
 	DeleteTestimonial(ctx context.Context, id string) error
 }
