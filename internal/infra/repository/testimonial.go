76a5e38c6178cf0bef569a848cf2552990e0386a
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-07-25 21:43:38 -0300

    [feat] add repository

--- internal/infra/repository/testimonial.go
+++ internal/infra/repository/testimonial.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"database/sql"
 
+	"github.com/google/uuid"
 	"github.com/rafaelmgr12/jornada-milha-api/internal/domain/entity"
 	"github.com/rafaelmgr12/jornada-milha-api/internal/infra/db"
 )
@@ -20,38 +21,45 @@ func NewTestimonialRepository(dbt *sql.DB) *TestimonialRepository {
 	}
 }
 
-func (t *TestimonialRepository) CreateTestimonial(ctx context.Context, testimonial entity.Testimonial) error {
+func (t *TestimonialRepository) CreateTestimonial(ctx context.Context, testimonial entity.Testimonial) (entity.Testimonial, error) {
 	err := t.Queries.CreateTestimonial(ctx, db.CreateTestimonialParams{
 		ID:          testimonial.ID.String(),
 		Name:        testimonial.Name,
 		Testimonial: testimonial.Testimonial,
 	})
 	if err != nil {
-		return err
+		return entity.Testimonial{}, err
 	}
-	return nil
+	return testimonial, nil
 }
 
-func (t *TestimonialRepository) GetTestimonials(ctx context.Context) ([]entity.Testimonial, error) {
+func (t *TestimonialRepository) ReadTestimonial(ctx context.Context) ([]entity.Testimonial, error) {
 	res, err := t.Queries.GetTestimonial(ctx)
 	if err != nil {
 		return nil, err
 	}
 	testimonials := make([]entity.Testimonial, len(res))
+	for i, testimonial := range res {
+		testimonials[i] = entity.Testimonial{
+			ID:          uuid.MustParse(testimonial.ID),
+			Name:        testimonial.Name,
+			Testimonial: testimonial.Testimonial,
+		}
+	}
 
 	return testimonials, nil
 }
 
-func (t *TestimonialRepository) UpdateTestimonial(ctx context.Context, testimonial entity.Testimonial) error {
+func (t *TestimonialRepository) UpdateTestimonial(ctx context.Context, testimonial entity.Testimonial) (entity.Testimonial, error) {
 	err := t.Queries.UpdateTestimonial(ctx, db.UpdateTestimonialParams{
 		ID:          testimonial.ID.String(),
 		Name:        testimonial.Name,
 		Testimonial: testimonial.Testimonial,
 	})
 	if err != nil {
-		return err
+		return entity.Testimonial{}, err
 	}
-	return nil
+	return testimonial, nil
 }
 
 func (t *TestimonialRepository) DeleteTestimonial(ctx context.Context, id string) error {
