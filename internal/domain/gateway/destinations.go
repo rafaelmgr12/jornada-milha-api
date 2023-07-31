bd0b238839f5c9c1a00b0527a0484b4543fd7a92
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-07-31 09:46:53 -0300

    [feat] add destinations gatewau

---
+++ internal/domain/gateway/destinations.go
@@ -0,0 +1,14 @@
+package gateway
+
+import (
+	"context"
+
+	"github.com/rafaelmgr12/jornada-milha-api/internal/domain/entity"
+)
+
+type DestinationsGateway interface {
+	CreateDestinations(ctx context.Context, destinations entity.Destinations) (entity.Destinations, error)
+	ReadDestinations(ctx context.Context) ([]entity.Destinations, error)
+	UpdateDestinations(ctx context.Context, destinations entity.Destinations) (entity.Destinations, error)
+	DeleteDestinations(ctx context.Context, id string) error
+}
