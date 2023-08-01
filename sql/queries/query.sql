7f4f6df10acab5d219bb7e2f6badefddc640ec8a
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-08-01 10:27:06 -0300

    [chore] feat sql

--- sql/queries/query.sql
+++ sql/queries/query.sql
@@ -11,18 +11,18 @@ UPDATE testimonials SET name = ?, testimonial = ? WHERE id = ?;
 DELETE FROM testimonials WHERE id = ?;
 
 -- name: CreateDestination :exec
-INSERT INTO destinations (id, name, price, photo) VALUES (?, ?, ?, ?);
+INSERT INTO destinations (id, photo1, photo2, name, meta, text_description, price) VALUES (?, ?, ?, ?, ?, ?, ?);
 
 -- name: GetDestination :many
-SELECT * FROM destinations ORDER BY name;
+SELECT id, photo1, photo2, name, meta, text_description, price FROM destinations ORDER BY name;
 
 -- name: UpdateDestination :exec
-UPDATE destinations SET name = ?, price = ?, photo = ? WHERE id = ?;
+UPDATE destinations SET photo1 = ?, photo2 = ?, name = ?, meta = ?, text_description = ?, price = ? WHERE id = ?;
 
 -- name: DeleteDestination :exec
 DELETE FROM destinations WHERE id = ?;
 
 -- name: GetDestinationsByName :many
-SELECT id, name, price, photo
+SELECT id, photo1, photo2, name, meta, text_description, price
 FROM destinations
 WHERE name LIKE CONCAT('%', ?, '%');
