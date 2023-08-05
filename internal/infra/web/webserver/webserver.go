93aeaab55c7d3d5d0852777fa156de48c203a1fb
Rafael Ribeiro <ribeirorafaelmatheus@gmail.com>
2023-08-05 11:37:50 -0300

    [feat] add prometheus

--- internal/infra/web/webserver/webserver.go
+++ internal/infra/web/webserver/webserver.go
@@ -6,6 +6,7 @@ import (
 	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/cors"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	httpSwagger "github.com/swaggo/http-swagger"
 )
 
@@ -15,6 +16,7 @@ type WebServer struct {
 }
 
 func NewWebServer(webServerPort string) *WebServer {
+
 	router := chi.NewRouter()
 
 	router.Use(cors.Handler(cors.Options{
@@ -25,11 +27,15 @@ func NewWebServer(webServerPort string) *WebServer {
 		AllowCredentials: false,
 	}))
 
+	router.Use(middleware.RequestID)
+	router.Use(middleware.RealIP)
 	router.Use(middleware.Logger)
 
 	router.Get("/swagger/*", httpSwagger.Handler(
 		httpSwagger.URL("http://localhost:8080/swagger/doc.json"), //The url pointing to API definition
 	))
+
+	router.Handle("/metrics", promhttp.Handler())
 
 	return &WebServer{
 		WebServerPort: webServerPort,
